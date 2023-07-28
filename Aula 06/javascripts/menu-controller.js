96fae2bcd41a2da4d38a50ab70176212b4b7d569
Camili <124635963+camiligomes@users.noreply.github.com>
2023-07-28 19:46:51 -0300

    Delete Aula 06 directory

--- Aula 06/javascripts/menu-controller.js
+++
@@ -1,6 +0,0 @@
-var toggleMenu = document.querySelector('.js-bar-button');
-
-toggleMenu.onclick = function() {
-	var menu = document.querySelector('.js-menu');
-	menu.classList.toggle('header-menu--toggling');
-}
